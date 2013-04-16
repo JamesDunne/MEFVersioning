86f584304e027a1115787c27254a2888c098f5dd
James Dunne <james.jdunne@gmail.com>
2013-04-15 19:52:31 -0500

    Initial commit of versioning solution using MEF.

---
+++ Versions/v1/API.Implementation/Students/StudentsCollection.cs
@@ -0,0 +1,17 @@
+﻿using API.Students;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Implementation.Students
+{
+    public sealed class StudentsCollection : IStudentsCollection
+    {
+        public async Task<Student> Get(StudentID id)
+        {
+            return new Student() { ID = id };
+        }
+    }
+}
---
+++ Versions/v1/API.Interface/Students/IStudentsCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Students
+{
+    public interface IStudentsCollection
+    {
+        Task<Student> Get(StudentID id);
+    }
+}
